8fe2a0614b66299199ec41f3652c07512c8f6394
Sandoerton <sandoerton@gmail.com>
2022-02-02 23:29:37 -0300

    Inicio das Praticas

---
+++ 1 Fundamentos Web/B4-Intro_JScript/Dia-4.1 JS_Primeiros_Passos/praticas.js
@@ -0,0 +1,38 @@
+//Primeira Pratica
+
+/*const myName = 'Sandoerton';
+const birthCity = 'Caruaru-PE';
+let birthYear = 1985;
+
+console.log(myName, birthCity, birthYear);
+
+birthYear = 2022;
+
+console.log(birthYear);*/
+
+//Segunda Pratica
+
+/*let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+
+patientId = '50';
+
+console.log(typeof patientId);*/
+
+//Terceira Pratica
+
+/*const base = 5;
+let height = 8;
+const area = base*height;
+const perimeter = 2*base + 2*height;
+
+console.log(area, perimeter);*/
+
+
