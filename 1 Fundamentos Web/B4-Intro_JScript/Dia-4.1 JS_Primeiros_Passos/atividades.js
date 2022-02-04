3f181dee098d8d9a3644e8fc34f12e9b8f941d02
Sandoerton <sandoerton@gmail.com>
2022-02-04 06:46:54 -0300

    Exerc 4 pronto

---
+++ 1 Fundamentos Web/B4-Intro_JScript/Dia-4.1 JS_Primeiros_Passos/atividades.js
@@ -0,0 +1,78 @@
+//Ativ 1
+/*
+let a = (Math.floor(Math.random()*100)+1);
+let b = (Math.floor(Math.random()*100)+1);
+let soma;
+let diminui;
+let multiplica;
+let divide;
+let sobra;
+
+soma = a + b;
+diminui = a - b;
+multiplica = a * b;
+divide = a / b;
+sobra = a % b;
+
+console.log('A vale: ', a);
+console.log('B vale: ', b);
+console.log('A soma é igual a : ', a+b);
+console.log('A subtração é igual a : ', a-b);
+console.log('A multiplicação é igual a : ', a*b);
+console.log('A divisão é igual a : ', a/b);
+console.log('O resto da divisão é : ', a%b);
+*/
+
+// Ativ 2
+
+/*
+const a = (Math.floor(Math.random()*100)+1);
+const b = (Math.floor(Math.random()*100)+1);
+
+console.log('A vale: ',a ,' e B vale: ', b);
+
+if (a > b) {
+    console.log('O maior valor é A');
+} else {
+    console.log('O maior valor é B');
+}
+*/
+
+//Ativ 3
+
+/*
+const a = (Math.floor(Math.random()*100)+1);
+const b = (Math.floor(Math.random()*100)+1);
+const c = (Math.floor(Math.random()*100)+1);
+
+console.log('A vale: ',a ,' e B vale: ', b, ' e C vale: ', c);
+
+if (a >= b && a >= c) {
+    console.log('O maior valor é A');
+} else if (b >= a && b >= c){
+    console.log('O maior valor é B');
+} else {
+    console.log('O maior valor é C')
+}
+*/
+
+//Ativ 4
+
+/*
+const b = 10;
+
+console.log('O valor escolhido foi: ', b);
+
+if (b > 0) {
+    console.log('O valor é positivo');
+} else if (b < 0) {
+    console.log('O valor é negativo');
+} else {
+    console.log('O valor é zero');
+}
+*/
+
+//Ativ 5
+
+
+
