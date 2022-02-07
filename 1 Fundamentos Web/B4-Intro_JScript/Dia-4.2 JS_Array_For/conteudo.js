9e4f00ffc3a2c8d730a6b1f7e7449423f44683cb
Sandoerton <sandoerton@gmail.com>
2022-02-07 15:31:22 -0300

    conteudo concluido

---
+++ 1 Fundamentos Web/B4-Intro_JScript/Dia-4.2 JS_Array_For/conteudo.js
@@ -0,0 +1,38 @@
+//Exerc 1
+/*
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+*/
+
+//Exerc 2
+/*
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortifolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortifolio);
+*/
+
+//Exerc 3
+/*
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('Contato');
+console.log(menu);
+*/
+
+//Exerc 4
+/*
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for (let i = 0; i < groceryList.length; i++) {
+    console.log(groceryList[i]);
+}
+*/
+
+//Exerc 5
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (const name of names) {
+    console.log(name);
+}
+
