ece969453b85a5b0d5b5bebf408e3865eab4349e
Sandoerton <sandoerton@gmail.com>
2022-02-13 18:39:29 -0300

    Enviando exercicios ate o 9

--- 1 Fundamentos Web/B5-JS_DOM_Events_WebStorage/Dia-5.2 Trab_Elementos/exercicio5.2/script.js
+++ 1 Fundamentos Web/B5-JS_DOM_Events_WebStorage/Dia-5.2 Trab_Elementos/exercicio5.2/script.js
@@ -4,3 +4,48 @@ let h1Ex = document.createElement('h1');
 h1Ex.innerText = 'Exercício 5.2 - JavaScript DOM';
 bodyEx.appendChild(h1Ex);
 
+let mainEx = document.createElement('main');
+mainEx.className = 'main-content';
+bodyEx.appendChild(mainEx);
+
+let sec_C = document.createElement('section');
+sec_C.className = 'center-content';
+mainEx.appendChild(sec_C);
+
+let p_Sec_C = document.createElement('p');
+p_Sec_C.innerText = 'Texto do paragrafo central';
+sec_C.appendChild(p_Sec_C);
+
+let sec_L = document.createElement('section');
+sec_L.className = 'left-content';
+mainEx.appendChild(sec_L);
+
+let sec_R = document.createElement('section');
+sec_R.className = 'right-content';
+mainEx.appendChild(sec_R);
+
+let imgSL = document.createElement('img');
+imgSL.src='https://picsum.photos/200';
+imgSL.className = 'small-image';
+sec_L.appendChild(imgSL);
+
+let ulSecRig = document.createElement('ul');
+
+let lista = ['um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez'];
+for (let i = 0; i < lista.length; i += 1) {
+    let item = lista[i];
+    let newItemLista = document.createElement('li');
+    newItemLista.innerText = item;
+    newItemLista.innerHTML = newItemLista.innerHTML.toUpperCase();
+    ulSecRig.appendChild(newItemLista);
+}
+sec_R.appendChild(ulSecRig);
+
+//let h3Nomes = ['h3n1', 'h3n2', 'h3n3'];
+for (let i = 0; i < 3; i += 1) {
+    //let nomeh3 = h3Nomes[i];
+    let newh3 = document.createElement('h3');
+    mainEx.appendChild(newh3);
+}
+
+
